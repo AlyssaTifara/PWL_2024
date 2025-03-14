6db1698091ac65b493c6175beaa7595571c47e61
AlyssaTifara <tissaalyssa@gmail.com>
2025-03-14 08:58:16 +0700

    pk2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,7 @@ class UserController extends Controller
         // $user = UserModel::findOr(20, ['username','nama'], function(){
         //     abort(404);
         // });
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
 
     }
