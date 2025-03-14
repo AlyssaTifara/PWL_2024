aa0b1152ccccb4468794912c7ee048c550cc4004
AlyssaTifara <tissaalyssa@gmail.com>
2025-03-14 08:30:50 +0700

    pk2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,16 +14,20 @@ class UserController extends Controller
         //     'nama' => 'Pelanggan Pertama',
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        // ];
+        // UserModel::create($data);
         
-        $user = UserModel::all();
+        // $user = UserModel::findOr(20, ['username','nama'], function(){
+        //     abort(404);
+        // });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
+
     }
     public function user()
     {
