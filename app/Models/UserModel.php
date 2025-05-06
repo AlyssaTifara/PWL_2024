8568859705b9eee0086b62120dc20b3359c694b1
AlyssaTifara <tissaalyssa@gmail.com>
2025-05-06 08:36:58 +0700

    multi_level

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -36,6 +36,11 @@ class UserModel extends Authenticatable
     {
         return $this->level->level_kode == $role;
     }
+
+    public function getRole()
+    {
+        return $this->level->level_kode;
+    }
 }
 
 
