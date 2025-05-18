379e9464601efaa53090de767d4a1bd9639ef836
AlyssaTifara <tissaalyssa@gmail.com>
2025-05-18 22:16:19 +0700

    jb 10_tugas

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\KategoriController;
+use App\Http\Controllers\Api\BarangController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,14 +22,43 @@ use App\Http\Controllers\Api\LogoutController;
 |
 */
 
-Route::post('/register', [RegisterController:: class, '__invoke'])->name('register');
-Route::post('/login', [LoginController:: class, '__invoke'])->name('login');
-Route::post('/logout', [LogoutController:: class, '__invoke'])->name('logout');
-Route::get('levels', [LevelController:: class, 'index']);
-Route::post('levels', [LevelController:: class, 'store' ]);
-Route::get('levels/{level}', [LevelController:: class, 'show' ]);
-Route::put('levels/{level}', [LevelController::class, 'update' ]);
-Route::delete('levels/{level}', [LevelController:: class, 'destroy']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::post('/register', [RegisterController:: class, '__invoke'])->name('registers');
+Route::post('/login', [LoginController:: class, '__invoke'])->name('logins');
+Route::post('/logout', [LogoutController:: class, '__invoke'])->name('logouts');
+
+//level
+Route::get('level', [LevelController:: class, 'index']);
+Route::post('level', [LevelController:: class, 'store' ]);
+Route::get('level/{level}', [LevelController:: class, 'show' ]);
+Route::put('level/{level}', [LevelController::class, 'update' ]);
+Route::delete('level/{level}', [LevelController:: class, 'destroy']);
+
+//user
+Route::get('user', [UserController::class, 'index']);
+Route::post('user', [UserController::class, 'store']);
+Route::get('user/{id}', [UserController::class, 'show']);
+Route::put('user/{id}', [UserController::class, 'update']);
+Route::delete('user/{id}', [UserController::class, 'destroy']);
+
+//kategori
+Route::get('kategori', [KategoriController::class, 'index']);
+Route::post('kategori', [KategoriController::class, 'store']);
+Route::get('kategori/{id}', [KategoriController::class, 'show']);
+Route::put('kategori/{id}', [KategoriController::class, 'update']);
+Route::delete('kategori/{id}', [KategoriController::class, 'destroy']);
+
+//barang
+Route::get('barang', [BarangController::class, 'index']);
+Route::post('barang', [BarangController::class, 'store']);
+Route::get('barang/{id}', [BarangController::class, 'show']);
+Route::put('barang/{id}', [BarangController::class, 'update']);
+Route::delete('barang/{id}', [BarangController::class, 'destroy']);
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->get('/profile', function (Request $request) {
     return $request->user();
 });
